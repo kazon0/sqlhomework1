62ba50c84d9f0d197e9e4cc69d2ceb2b7f70d89a
kazon0 <142081446+kazon0@users.noreply.github.com>
2025-06-28 22:19:18 +0800

    Initial Commit

---
+++ sqlhomework1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sqlhomework1
+//
+//  Created by 郑金坝 on 2025/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sqlhomework1/sqlhomework1App.swift
@@ -0,0 +1,17 @@
+//
+//  sqlhomework1App.swift
+//  sqlhomework1
+//
+//  Created by 郑金坝 on 2025/6/28.
+//
+
+import SwiftUI
+
+@main
+struct sqlhomework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
